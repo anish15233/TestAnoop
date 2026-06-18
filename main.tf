39fb5c64f57342edd671f9775fab6cd4677a16d4
Anish Singh <anish15233@gmail.com>
2026-06-18 20:45:57 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+resourc "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["
+  }
+  
